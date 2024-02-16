85ea0f23824eaf16f306bd1beab3925c75bd2ce7
J-C <cricriclouatre@gmail.com>
2024-02-16 00:11:27 -0500

    Remove express-validator from activitiesController.ts
    The express-validation module has been removed from activitiesController.ts and error handling has been simplified. Instead of validating the request and returning a 400 status code with the errors, the function now directly creates an activity without any preliminary validations.

--- backend/src/controllers/activitiesController.ts
+++ backend/src/controllers/activitiesController.ts
@@ -1,14 +1,10 @@
 import {NextFunction, Request, Response} from 'express';
 import { activities } from '../models/activities';
-import { validationResult } from 'express-validator';
 import { db } from '../db/db';
 
 // TODO faire de plus ample vérification sur les données d'un activité
 export const createActivity = async (req: Request, res: Response, next: NextFunction) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
+
 
   try {
     const { userId, name, city, type, date, durationTotal, distanceTotal, comment, segments } = req.body;
