30943bd37ea0a70a84a998dac6fcd4bab8172543
J-C <cricriclouatre@gmail.com>
2024-02-29 12:10:29 -0500

    Refactor createActivity function in activitiesController
    The createActivity function was revised to improve data handling and error reporting. Newly added default values for 'date' and 'segments' prevent potential null or undefined values. Enhanced user ID extraction and error handling ensure smoother database insertion of a new activity.

--- backend/src/controllers/activitiesController.ts
+++ backend/src/controllers/activitiesController.ts
@@ -4,18 +4,41 @@ import { db } from '../db/db';
 
 // TODO faire de plus ample vérification sur les données d'un activité
 export const createActivity = async (req: Request, res: Response, next: NextFunction) => {
-
-
   try {
-    const { userId, name, city, type, date, durationTotal, distanceTotal, comment, segments } = req.body;
+    let { name, city, type, date, durationTotal, distanceTotal, comment, segments } = req.body;
 
-    await db.insert(activities).values([{
-      user_id: userId, name, city, type, date, durationTotal, distanceTotal, comment, segments
+
+    if (!date)
+      date = new Date();
+    else
+      date = new Date(date);
+
+    if (!segments || segments.trim() === '')
+      segments = {};
+
+    const userId = req.user?.userId as number;
+
+    const result = await db.insert(activities).values([{
+      user_id: userId,
+      name,
+      city,
+      type,
+      date,
+      durationTotal,
+      distanceTotal,
+      comment,
+      segments
     }]);
+
+    if (!result) {
+      // handle the error or throw an error
+      throw new Error('Database insertion failed.');
+    }
 
 
     return res.status(201).json({ message: 'Activity added successfully' });
   } catch (error) {
+    console.log(error);
     next(error);
   }
 };
