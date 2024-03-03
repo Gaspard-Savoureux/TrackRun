70d2ae1f3ff5f179bbfbc638dc650b0ae9eb67f5
Marc-Antoine Lavallée <lavallee.marc-antoine.2@courrier.uqam.ca>
2024-03-02 21:57:33 -0500

    Test d'intégration

---
+++ backend/__tests__/routes/planned_activity.test.ts
@@ -0,0 +1,69 @@
+import {closeDbConnection, db} from '../../src/db/db';
+import {users} from '../../src/models/users';
+import {planned_activities} from '../../src/models/planned_activities';
+import { eq } from 'drizzle-orm';
+import request from 'supertest';
+import app from '../../src/app';
+import { except } from 'drizzle-orm/mysql-core';
+
+const user = {username: 'test-user', password: '1234'};
+var auth_token:string;
+
+async function create_planned_activities() {
+    
+}
+
+beforeAll(async () => {
+  // Cleans DB
+  await db.delete(planned_activities);
+  await db.delete(users);
+  // Create user
+  await request(app).post('/user/create').send(user);
+  // Get auth token
+  auth_token = (await request(app).post('/auth').send(user)).body['token'];
+});
+
+afterAll(async () => {
+  // Cleans DB
+  await db.delete(planned_activities);
+  await db.delete(users);
+  return closeDbConnection();
+});
+
+describe('GET PlannedActivities', () => {
+  const route : string = '/plannedactivities';
+  test('Should return 401: Unauthorized', async () => {
+    const res = await request(app)
+      .get(route);
+    expect(res.status).toBe(401);
+  });
+  test('Should return empty set', async () => {
+    const res = await request(app)
+      .get(route)
+      .set('Authorization', 'Bearer ' + auth_token);
+    expect(res.body).toMatchObject({'plannedActivities': []});
+  });
+  
+  test('Should return set with one element', async () => {
+    // TEMPORARY: Create some planned_activities until route is available
+    const userid = (await db.select().
+      from(users).
+      where(eq(users.username, user.username)))[0].id;
+
+    await db.insert(planned_activities).values({user_id: userid, 
+                                                type: 'Running', 
+                                                date: new Date(2024, 1, 12), 
+                                                duration: 3600});
+    const res = await request(app)
+      .get(route)
+      .set('Authorization', 'Bearer ' + auth_token);
+      //expect(res.body).toMatchObject({'plannedActivities': []});
+      expect(res.body).toEqual({
+        'plannedActivities': [expect.objectContaining({
+          //Dates might fail depending on timezones
+          "type": "Running",
+          "duration": 3600,
+          })]
+        });
+      })
+    });
--- backend/src/models/planned_activities.ts
+++ backend/src/models/planned_activities.ts
@@ -1,4 +1,5 @@
 import { int, varchar, datetime, text, mysqlTable, mysqlEnum } from 'drizzle-orm/mysql-core';
+import {relations } from 'drizzle-orm'
 import { users } from './users';
 import { activities } from './activities';
 
@@ -50,13 +51,24 @@ import { activities } from './activities';
  */
 export const planned_activities = mysqlTable('planned_activities', {
   id: int('id').primaryKey().autoincrement(),
-  user_id: int('user_id').references(() => users.id).notNull(),  
-  type: mysqlEnum('type', ['Running', 'Biking', 'Walking']).notNull(), // TO FIX: For activities, string is used instead
+  user_id: int('user_id').notNull(),  
+  type: mysqlEnum('type', ['Running', 'Biking', 'Walking']).notNull(),
   date: datetime('date').notNull(),
   duration: int('duration').notNull(),
   name: varchar('name', { length: 256 }),
   comment: text('comment'),
-  activity_id: int('activity_id').references(() => activities.id)
+  activity_id: int('activity_id').references(() => activities.id) // Make relation instead
 });
 
-export type PlannedActivities = typeof planned_activities.$inferInsert;
+export const usersRelations = relations(users, ({ many }) => ({
+    planned_activities: many(planned_activities),
+}));
+
+export const planActRelations = relations(planned_activities, ({ one }) => ({
+    user: one(users, {
+        fields: [planned_activities.user_id],
+        references: [users.id],
+    }),
+}));
+
+export type PlannedActivity = typeof planned_activities.$inferInsert;
