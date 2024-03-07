b9ffcc62d5b80d8dc648684670efca2c3d0df0bb
Lajoie, Samuel <lajoie.samuel@courrier.uqam.ca>
2024-03-07 13:09:58 -0500

    ADD composante modale informative qui affiche de l'information pour un élément voulu

---
+++ frontend/src/lib/components/informative-modal.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  export let href = '#';
+  export let info = '';
+
+  let showModal = false;
+</script>
+
+<a {href} on:mouseenter={() => (showModal = true)} on:mouseleave={() => (showModal = false)}>
+  <slot />
+
+  {#if showModal}
+    <div class="modal">{info}</div>
+  {/if}
+</a>
+
+<style>
+  .modal {
+    position: absolute;
+    text-decoration: none;
+    color: var(--text);
+    background-color: var(--bg-2);
+    border-radius: 0.2rem;
+    padding: 0.3rem;
+  }
+
+  a:link {
+    text-decoration: none;
+    color: var(--text);
+  }
+</style>
