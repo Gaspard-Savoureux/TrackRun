5bfe18eba216d3541eed739ddbb52937968f7518
Marc-Antoine Lavallée <lavallee.marc-antoine.2@courrier.uqam.ca>
2024-03-07 23:33:52 -0500

    It is sketchy but it works. To fix: the type plannedActivity doesn't seem to apply implictly from JSON... Also, I think I should use onMount instead of load to fetch data. Will be more reusable I think. Especially if filter need to change dynamically...

---
+++ frontend/src/lib/types/plannedActivity.ts
@@ -0,0 +1,9 @@
+export type PlannedActivity = {
+  id: number;
+  type: string;
+  date: string;
+  duration: number;
+  name: string;
+  comment: string;
+  activity_id: number | null;
+}
--- frontend/src/routes/plannedActivities/+page.server.ts
+++ frontend/src/routes/plannedActivities/+page.server.ts
@@ -1,61 +1,19 @@
 // src/routes/plannedActivities/+page.server.ts
+import {API_URL} from '../../constants';
+import type { PageServerLoad } from '../$types'
+import type { PlannedActivity } from '$lib/types/plannedActivity';
 
-export type PlannedActivity = {
-  id: number;
-  type: string;
-  date: string;
-  duration: number;
-  name: string;
-  comment: string;
-  activity_id: number | null;
-};
+// To create activities in DB quickly
+// insert into planned_activities(user_id, type, date, duration, name) values (60, 'Running', '2024-02-12 00:00:00', 3600, 'A good run!');
 
-// Static JSON data
-const staticPlannedActivities: PlannedActivity[] = [
-  {
-    id: 1,
-    type: 'Running',
-    date: '2024-02-26 16:30:00',
-    duration: 1800,
-    name: 'A run in the park',
-    comment: '',
-    activity_id: null,
-  },
-  {
-    id: 2,
-    type: 'Running',
-    date: '2024-02-27 16:30:00',
-    duration: 1200,
-    name: 'A run in the park again',
-    comment: '',
-    activity_id: null,
-  },
-  {
-    id: 3,
-    type: 'Biking',
-    date: '2024-03-02 19:00:00',
-    duration: 3600,
-    name: 'Biking around the block',
-    comment: 'Be carefull with your knee!',
-    activity_id: null,
-  },
-  {
-    id: 4,
-    type: 'Walking',
-    date: '2024-03-05 13:00:00',
-    duration: 2400,
-    name: 'Smooth walk',
-    comment: 'Enjoy your walk!',
-    activity_id: null,
-  },
-];
+const pActivitiesUrl = `${API_URL}/plannedactivities`
 
-export async function load(): Promise<{ plannedActivities: PlannedActivity[] }> {
-  return {
-    props: {
-      plannedActivities: staticPlannedActivities,
-    },
-  };
-  
+export const load: PageServerLoad = async ({ fetch, locals }) => {
+    const res = await fetch(pActivitiesUrl, {
+        method: 'GET',
+        headers: { Authorization: `Bearer ${locals.token}` }
+    });
+    const plannedActivities: PlannedActivity[] = await res.json();
+    return { plannedActivities } 
+   
 }
-
