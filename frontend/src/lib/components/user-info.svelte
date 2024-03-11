93188287f2575babf46bdad23b6f7b6915728921
Lajoie, Samuel <lajoie.samuel@courrier.uqam.ca>
2024-03-11 18:32:55 -0400

    Factorisation des user-infos

---
+++ frontend/src/lib/components/user-info.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import Item from '$lib/components/userinfo-field.svelte';
+  import type { User } from '$lib/types/user';
+
+  export let user: User;
+</script>
+
+<div class="column">
+  <div class="row">
+    <Item name="Username">{user?.username}</Item>
+    <Item name="Email">{user?.email}</Item>
+  </div>
+  <div class="row">
+    <Item name="Age">{user?.age}</Item>
+    <Item name="Sex">{user?.sex}</Item>
+  </div>
+  <div class="row">
+    <Item name="Height">{user?.height && user?.height + ' cm'}</Item>
+    <Item name="Weight">{user?.weight && user?.weight + ' kg'}</Item>
+  </div>
+  <Item name="Description">{user?.description}</Item>
+</div>
+
+<style>
+  .column {
+    padding: 1rem;
+    max-width: 40rem;
+    margin: 0 auto;
+    border-radius: 0.35rem;
+    background-color: var(--bg-2);
+    display: flex;
+    flex-flow: column wrap;
+  }
+
+  @media (width >= 500px) {
+    .column {
+      padding: 2rem;
+    }
+  }
+
+  .row {
+    display: flex;
+    flex-flow: row wrap;
+  }
+</style>
