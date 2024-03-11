cdf3afc322e57e616b680087239e24d0f9588fe1
Gazzé, Maxime <gazze.maxime@courrier.uqam.ca>
2024-03-11 15:16:23 -0400

    ADD settings page

---
+++ frontend/src/routes/settings/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import ThemeSwitcher from "$lib/components/theme-switcher.svelte";
+
+
+</script>
+
+<svelte:head>
+  <title>Settings</title>
+</svelte:head>
+
+<section>
+  <h1>Update user profile</h1>
+  <form method="PUT">
+    <label>
+      Username
+      <input type="text" name="username" />
+    </label>
+    <label>
+      Password
+      <input type="password" name="password" />
+    </label>
+    <label>
+      Age
+      <input type="text" name="age" />
+    </label>
+    <label>
+      Height
+      <input type="text" name="height" />
+    </label>
+    <label>
+      Weight
+      <input type="text" name="weight" />
+    </label>
+    <button type="submit">Update</button>
+    <a href="/profile">Cancel</a>
+  </form>
+  <ThemeSwitcher />
+  <a href="/logout">logout</a>
+</section>
+
+<style>
+
+</style>
