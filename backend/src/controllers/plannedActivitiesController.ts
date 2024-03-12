c56cd08c15ce93623f242373ef1dc82ad645b59c
Pascal Thibodeau <thibodeau.pascal.2@courrier.uqam.ca>
2024-03-11 20:37:16 -0400

    modification de delete mis dnas un service et verification bon userid pour supression

--- backend/src/controllers/plannedActivitiesController.ts
+++ backend/src/controllers/plannedActivitiesController.ts
@@ -1,9 +1,10 @@
-import {NextFunction, Request, Response} from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { planned_activities } from '../models/planned_activities';
 import { User } from '../models/users';
-import {getUserById} from '../services/user.services';
+import { getUserById } from '../services/user.services';
 import { db } from '../db/db';
 import { eq } from 'drizzle-orm';
+import { deletePlannedActivityById, selectPlannedActivityById } from '../services/planned_activity.services';
 
 export const getPlannedActivities = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -30,17 +31,23 @@ export const getPlannedActivities = async (req: Request, res: Response, next: Ne
 
 export const deletePlannedActivity = async (req: Request, res: Response, next: NextFunction) => {
   try {
-
-    // Check if activityId is a number
+    const userId = req.user?.userId as number;
     const activityId = parseInt(req.params.activityId);
-    if (isNaN(activityId)) {
+
+    // check if the activityId is a number
+    if (isNaN(activityId) || !activityId) {
       return res.status(400).json({ error: "Invalid activityId" });
     }
+    const activitiesToDelete = await selectPlannedActivityById(activityId, userId);
+    // log to delete
+    console.log(activitiesToDelete);
+    if (activitiesToDelete.length !== 1) {
+      return res.status(404).json({ error: "No corresponding activity" });
+    }
 
-    // delete the activity
-    await db.delete(planned_activities).where(eq(planned_activities.id, activityId));
+    await deletePlannedActivityById(activityId, userId);
     res.status(200).json({ message: "Activity deleted successfully" });
-  
+
   } catch (error) {
     next(error);
   }
