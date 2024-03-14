9a3a652a2281daac1fc8f9e0d40b27d236076d13
J-C <cricriclouatre@gmail.com>
2024-03-13 23:23:17 -0400

    Refactor GPX parsing to use promises
    Replaced callback-based FS operations with async/await and promises for reading and parsing GPX files. Removed deprecated code and implemented helper functions to read and convert GPX content to JSON.

--- backend/src/controllers/gpxParser.ts
+++ backend/src/controllers/gpxParser.ts
@@ -1,6 +1,58 @@
-import { Parser } from 'xml2js';
-import * as fs from 'fs';
+import {Parser, parseStringPromise} from 'xml2js';
 import {getDistance} from 'geolib';
+import { readFile } from 'fs/promises';
+
+
+interface GpxPoint {
+  $: {
+    lat: string;
+    lon: string;
+  };
+  ele: string[];
+  time: string[];
+}
+
+interface GpxTrkSeg {
+  trkpt: GpxPoint[];
+}
+
+interface GpxTrk {
+  name: string[];
+  type: string[];
+  trkseg: GpxTrkSeg[];
+}
+
+interface Gpx {
+  gpx: {
+    trk: GpxTrk[];
+    metadata: [
+      {
+        time: string[];
+      }
+    ];
+  };
+}
+
+async function readGpxFileAsString(filePath: string): Promise<string> {
+  try {
+    // Read the file content as a string.
+    const fileContent: string = await readFile(filePath, { encoding: 'utf8' });
+    return fileContent;
+  } catch (error) {
+    console.error('Error reading the file:', error);
+    throw error; // Rethrow the error for further handling if necessary.
+  }
+}
+
+async function convertGpxToJson(gpxContent: string): Promise<Gpx> {
+  try {
+    const result: Gpx = await parseStringPromise(gpxContent);
+    return result;
+  } catch (error) {
+    console.error('Error parsing GPX content:', error);
+    throw error;
+  }
+}
 
 export class gpxParser {
   private parser: Parser;
@@ -10,68 +62,89 @@ export class gpxParser {
   }
 
   public async parseGpxFile(filePath: string): Promise<any> {
-    return new Promise(async (resolve, reject) => {
+    const gpxContent = await readGpxFileAsString(filePath);
+    const gpxJson = await convertGpxToJson(gpxContent);
+    const result = {
+      metadata: {
+        durationTotal: await this.getTotalDuration(gpxJson.gpx.trk[0].trkseg),
+        distanceTotal: await this.getTotalDistance(gpxJson.gpx.trk[0].trkseg),
+        date: gpxJson.gpx.metadata[0].time
+      },
+      segments: gpxJson.gpx.trk[0].trkseg
+    };
+    return result;
+    /*    return new Promise((resolve, reject) => {
       fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
+          console.log('step2');
           reject(err);
         } else {
+          console.log('step3');
           this.parser.parseString(data, async (error: any, result: any) => {
             if (error) {
+              console.log('step4');
               reject(error);
             } else {
+              console.log('step5');
               try {
-                // calculate total distance and duration
-                const totalDistance = await this.getTotalDistance(filePath);
-                const totalDuration = await this.getTotalDuration(filePath);
-
-                // add to result
-                result.totalDistance = totalDistance;
-                result.totalDuration = totalDuration;
+                // Previously, here was a recursive call to parseGpxFile
+                // Now directly calculate total distance and duration from `result`
+                console.log('step6');
+                // const totalDistance = await this.getTotalDistance(data);
+                console.log('step7');
+                // const totalDuration = await this.getTotalDuration(data);
+                console.log('step8');
+                // Add to result
+                // result.totalDistance = totalDistance;
+                // result.totalDuration = totalDuration;
 
                 resolve(result);
+                console.log('step9');
               } catch (err) {
                 reject(err);
               }
             }
           });
+          console.log('step10');
         }
       });
-    });
+    });*/
   }
 
-  public async getTotalDistance(filePath: string): Promise<number> {
-    const data = await this.parseGpxFile(filePath);
+
+  private async getTotalDistance(data: GpxTrkSeg[]): Promise<number> {
     let totalDistance = 0;
-    for (let i = 1; i < data.trk.trkseg.trkpt.length; i++) {
-      const point1 = {
-        latitude: data.trk.trkseg.trkpt[i - 1].$.lat,
-        longitude: data.trk.trkseg.trkpt[i - 1].$.lon,
-      };
-      const point2 = {
-        latitude: data.trk.trkseg.trkpt[i].$.lat,
-        longitude: data.trk.trkseg.trkpt[i].$.lon,
-      };
-      // Calculating 2D distance
-      const distance2D = getDistance(point1, point2);
+    for (let i = 0; i < data.length; i++) {
+      for (let j = 1; j < data[i].trkpt.length; j++) {
+        const point1 = {
+          latitude: data[i].trkpt[j - 1].$.lat,
+          longitude: data[i].trkpt[j - 1].$.lon,
+        };
+        const point2 = {
+          latitude: data[i].trkpt[j].$.lat,
+          longitude: data[i].trkpt[j].$.lon,
+        };
+        const distance2D = getDistance(point1, point2);
 
-      // Getting elevation difference and converting it to meters if it's not
-      const elev1 = data.trk.trkseg.trkpt[i - 1].ele;
-      const elev2 = data.trk.trkseg.trkpt[i].ele;
-      const elevDifference = Math.abs(elev2 - elev1);
+        const elev1 = data[i].trkpt[j - 1].ele;
+        const elev2 = data[i].trkpt[j].ele;
+        const elevDifference = Math.abs(parseFloat(elev1.join('')) - parseFloat(elev2.join('')));
 
-      // Calculating 3D distance
-      totalDistance += Math.sqrt(Math.pow(distance2D, 2) + Math.pow(elevDifference, 2));
+        totalDistance += Math.sqrt(Math.pow(distance2D, 2) + Math.pow(elevDifference, 2));
+      }
     }
     return totalDistance;
   }
 
-  public async getTotalDuration(filePath: string): Promise<number> {  // new function
-    const data = await this.parseGpxFile(filePath);
-    const startTime = new Date(data.trk.trkseg.trkpt[0].time);
-    const endTime = new Date(data.trk.trkseg.trkpt[data.trk.trkseg.trkpt.length - 1].time);
-    const totalDuration = Math.abs(endTime.getTime() - startTime.getTime()) / 1000;  // duration in seconds
+  private async getTotalDuration(data: GpxTrkSeg[]): Promise<number> {
+    console.log(data);
+    let totalDuration = 0;
+    for (let i = 0; i < data.length; i++) {
+      const length = data[i].trkpt.length;
+      const startTime = new Date(data[i].trkpt[0].time.join(''));
+      const endTime = new Date(data[i].trkpt[length - 1].time.join(''));
+      totalDuration += Math.abs(endTime.getTime() - startTime.getTime()) / 1000;
+    }
     return totalDuration;
   }
-  
 }
-
