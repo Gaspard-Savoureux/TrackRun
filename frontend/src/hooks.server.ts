b88ad578446cf95ded2c83fd0a2b27cf03cd47a6
Gazzé, Maxime <gazze.maxime@courrier.uqam.ca>
2024-03-16 03:12:03 -0400

    ADD readd login guard

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,13 +1,12 @@
-// import { redirect, type Handle } from '@sveltejs/kit';
-import { type Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
   const token = event.cookies.get('token');
 
-  // const unguardedRoutes = ['/login', '/register'];
-  // if (!token && !unguardedRoutes.includes(event.url.pathname)) {
-  //   redirect(302, `/login?next=${encodeURIComponent(event.url.pathname + event.url.search)}`);
-  // }
+  const unguardedRoutes = ['/login', '/register'];
+  if (!token && !unguardedRoutes.includes(event.url.pathname)) {
+    redirect(302, `/login?next=${encodeURIComponent(event.url.pathname + event.url.search)}`);
+  }
 
   event.locals.token = token;
 
