9f79aca88b5d94b207dda341ad79e789c0277d65
J-C <cricriclouatre@gmail.com>
2024-03-19 13:38:35 -0400

    Refactor activity validation logic
    Abstracted field validation for activities into separate functions to streamline the activitiesController and improve code modularity. Utilized the new functions in place of inline validation, enhancing readability and maintainability.

--- backend/src/controllers/activitiesController.ts
+++ backend/src/controllers/activitiesController.ts
@@ -10,9 +10,14 @@ import {
   updateActivityById
 } from '../services/activity.services';
 import { gpxParser } from '../middlewares/gpxParser';
-import * as fs from 'node:fs';
 import {deletefile} from '../middlewares/fileTreatment';
-import {and, eq} from 'drizzle-orm';
+import {
+  validateCity, validateComment, validateDate,
+  validateDistance,
+  validateDuration,
+  validateName,
+  validateType
+} from '../middlewares/activityValidation';
 
 
 /**
@@ -30,40 +35,39 @@ export const createActivityManual = async (req: Request, res: Response, next: Ne
     let { name, city, type, date, durationTotal, distanceTotal, comment} = req.body;
 
     // Validation de `name`
-    if (!name || typeof name !== 'string' || name.length < 3 || name.length > 256) {
+    if (validateName(name)) {
       return res.status(400).json({ message: 'Name is required and must be between 3 and 256 characters' });
     }
 
     // Validation de `city`
     if (city == null) {
       city = '';
-    } else if (city && (typeof city !== 'string' || city.length > 100)) {
+    } else if (validateCity(city)) {
       return res.status(400).json({ message: 'City must be a string with a maximum length of 100 characters' });
     }
 
     // Validation de `type`
-    const validTypes = ['Running', 'Biking', 'Walking'];
-    if (!type || !validTypes.includes(type)) {
-      return res.status(400).json({ message: `Type is required and must be one of the following: ${validTypes.join(', ')}` });
+    if (validateType(type)) {
+      return res.status(400).json({ message: 'Type is required and must be one of the following: Running, Biking, Walking' });
     }
 
     // Validation de `durationTotal` et `distanceTotal`
     if (durationTotal == null) {
       durationTotal = 0;
-    } else if (typeof durationTotal !== 'number' || durationTotal <= 0) {
-      return res.status(400).json({ message: 'DurationTotal is required and must be a non-negative number' });
+    } else if (validateDuration(durationTotal)) {
+      return res.status(400).json({ message: 'DurationTotal must be a non-negative and non-null number' });
     }
 
     if (distanceTotal == null) {
       distanceTotal = 0;
-    } else if (typeof distanceTotal !== 'number' || distanceTotal <= 0) {
-      return res.status(400).json({ message: 'DistanceTotal is required and must be a non-negative number' });
+    } else if (validateDistance(distanceTotal)) {
+      return res.status(400).json({ message: 'DistanceTotal must be a non-negative and non-null number' });
     }
 
     // Validation de `comment` 
     if (comment == null) {
       comment = '';
-    } else if (comment && typeof comment !== 'string') {
+    } else if (validateComment(comment)) {
       return res.status(400).json({ message: 'Comment must be a string' });
     }
 
@@ -71,6 +75,8 @@ export const createActivityManual = async (req: Request, res: Response, next: Ne
     // Validation date
     if (!date)
       date = new Date();
+    else if (validateDate(date))
+      return res.status(400).json({ message: 'The date must be past'});
     else
       date = new Date(date);
 
@@ -118,19 +124,18 @@ export const createActivityGPX = async (req: Request, res: Response, next: NextF
     let { name, type, comment } = req.body;
 
     // Validation for `name`
-    if (!name || typeof name !== 'string' || name.length < 3 || name.length > 256) {
+    if (validateName(name)) {
       return res.status(400).json({ message: 'Name is required and must be between 3 and 256 characters' });
     }
     // Validation for `type`
-    const validTypes = ['Running', 'Biking', 'Walking'];
-    if (!type || !validTypes.includes(type)) {
-      return res.status(400).json({ message: `Type is required and must be one of the following: ${validTypes.join(', ')}` });
+    if (validateType(type)) {
+      return res.status(400).json({ message: 'Type is required and must be one of the following: Running, Biking, Walking'});
     }
 
     // Validation for `comment`
     if (comment == null) {
-      comment = ' ';
-    } else if (comment && typeof comment !== 'string') {
+      comment = '';
+    } else if (validateComment(comment)) {
       return res.status(400).json({ message: 'Comment must be a string' });
     }
 
@@ -149,11 +154,14 @@ export const createActivityGPX = async (req: Request, res: Response, next: NextF
       return res.status(400).send('No GPX file uploaded or wrong format');
     }
 
-    const city = null; // Assuming city is not determined from the GPX file
+    const city = null; //TODO get city in the GPX file from the coordinate
     let { date: date, durationTotal, distanceTotal } = metadata;
 
+    // Validation date
     if (!date)
       date = new Date();
+    else if (validateDate(date))
+      return res.status(400).json({ message: 'The date must be past\nThe GPX file isn\'t right'});
     else
       date = new Date(date);
 
@@ -338,15 +346,14 @@ export const modifyActivity = async (req: Request, res: Response, next: NextFunc
     }
     const { name, city, type, date, durationTotal, distanceTotal, comment} = req.body;
 
-    if (name && typeof name === 'string' && name.length >= 3 && name.length <= 256) updateData.name = name;
-    if (city && typeof city === 'string' && city.length <= 100) updateData.city = city; //TODO à changer quand la recup de la ville est fait dans le GPX
-    const activityOptions = ['Running', 'Biking', 'Walking'];
-    if (type && activityOptions.includes(type)) updateData.type = type;
-    if (typeof comment === 'string' && comment) updateData.comment = comment;
+    if (validateName(name)) updateData.name = name;
+    if (validateCity(city)) updateData.city = city; //TODO à changer quand la recup de la ville est fait dans le GPX
+    if (validateType(type)) updateData.type = type;
+    if (validateComment(comment)) updateData.comment = comment;
     if (activityRequest[0].segments === '{}') {
-      if (date) updateData.date = new Date(date);
-      if (durationTotal && typeof durationTotal === 'number' && durationTotal > 0) updateData.durationTotal = durationTotal;
-      if (distanceTotal && typeof distanceTotal === 'number' && distanceTotal > 0) updateData.distanceTotal = distanceTotal;
+      if (validateDate(date)) updateData.date = new Date(date);
+      if (validateDuration(durationTotal)) updateData.durationTotal = durationTotal;
+      if (validateDistance(distanceTotal)) updateData.distanceTotal = distanceTotal;
     }
 
     await updateActivityById(activityId, userId, updateData);
