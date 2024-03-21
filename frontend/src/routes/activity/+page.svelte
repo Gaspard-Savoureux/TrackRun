f8fbe8c4b4d34aa1bfb19dd0537e74267b6197e3
J-C <cricriclouatre@gmail.com>
2024-03-20 23:43:53 -0400

    Refactor page component and fix form export
    Removed redundant blank lines and corrected the position of the form export statement in +page.svelte. Ensured consistent indentation and a cleaner code structure for better readability.

--- frontend/src/routes/activity/+page.svelte
+++ frontend/src/routes/activity/+page.svelte
@@ -4,9 +4,7 @@
 
   let afficherManuelForm = false;
   let afficherGPXForm = false;
-  
-  
-  
+
   function afficherManuel() {
     afficherManuelForm = true;
     afficherGPXForm = false;
@@ -16,6 +14,7 @@
     afficherManuelForm = false;
     afficherGPXForm = true;
   }
+  export let form: { success?: boolean, message?: string } = {};
 
   export let data;
   const activities = data.activities.userActivities;
@@ -25,7 +24,7 @@
     if (!isConfirmed) {
       event.preventDefault();
     }
-  
+
   }
 
   function confirmerEtSupprimer(event: SubmitEvent) {
@@ -33,10 +32,9 @@
     if (!isConfirmed) {
       event.preventDefault();
     }
-    export let form: { success?: boolean, message?: string } = {};
 
-    export let data;
-    const activities = data.activities.userActivities;
+  }
+
 
 </script>
 
@@ -47,58 +45,59 @@
 <button on:click={afficherGPX}>Enregistrement GPX</button>
 
 {#if afficherManuelForm}
-    <ManuelForm />
+  <ManuelForm />
 {/if}
 
 {#if afficherGPXForm}
-    <GPXForm />
+  <GPXForm />
 {/if}
 
 {#if form?.success === false}<p class="danger">{form?.message}</p>{/if}
 
-  {#if activities.length > 0}
-    <h2>Activités enregistrées</h2>
-    {#each activities as activity}
-      <div class="activity">
-        <h3>{activity.name}</h3>
-        <p>Distance: {activity.distanceTotal}</p>
-        <p>Durée: {activity.durationTotal}</p>
-        <p>Date: {activity.date}</p>
-        <div class="options">
-          <form on:submit|preventDefault={confirmerEtModifier} method="POST" action="?/modifierActivite">
-            <input type="hidden" name="activityId" value="{activity.id}">
-            <input type="hidden" name="name" value="{activity.name}">
-            <input type="hidden" name="city" value="{activity.city}">
-            <input type="hidden" name="type" value="{activity.type}">
-            <input type="hidden" name="date" value="{activity.date}">
-            <input type="hidden" name="durationTotal" value="{activity.durationTotal}">
-            <input type="hidden" name="distanceTotal" value="{activity.distanceTotal}">
-            <input type="hidden" name="comment" value="{activity.comment}">
+{#if activities.length > 0}
+  <h2>Activités enregistrées</h2>
+  {#each activities as activity}
+    <div class="activity">
+      <h3>{activity.name}</h3>
+      <p>Distance: {activity.distanceTotal}</p>
+      <p>Durée: {activity.durationTotal}</p>
+      <p>Date: {activity.date}</p>
+      <div class="options">
+        <form on:submit|preventDefault={confirmerEtModifier} method="POST" action="?/modifierActivite">
+          <input type="hidden" name="activityId" value="{activity.id}">
+          <input type="hidden" name="name" value="{activity.name}">
+          <input type="hidden" name="city" value="{activity.city}">
+          <input type="hidden" name="type" value="{activity.type}">
+          <input type="hidden" name="date" value="{activity.date}">
+          <input type="hidden" name="durationTotal" value="{activity.durationTotal}">
+          <input type="hidden" name="distanceTotal" value="{activity.distanceTotal}">
+          <input type="hidden" name="comment" value="{activity.comment}">
 
-            <button type="submit">Modifier</button>
-          </form>
-          <form on:submit|preventDefault={confirmerEtSupprimer} method="POST" action="?/supprimerActivite">
-            <input type="hidden" name="activityId" value="{activity.id}">
-            <button type="submit">Supprimer</button>
-          </form>
-        </div>
+          <button type="submit">Modifier</button>
+        </form>
+        <form on:submit|preventDefault={confirmerEtSupprimer} method="POST" action="?/supprimerActivite">
+          <input type="hidden" name="activityId" value="{activity.id}">
+          <button type="submit">Supprimer</button>
+        </form>
       </div>
-    {/each}
-  {:else}
-    <p>Aucune activité enregistrée pour le moment.</p>
-  {/if}
+    </div>
+  {/each}
+{:else}
+  <p>Aucune activité enregistrée pour le moment.</p>
+{/if}
 
-  <style>
-    .options {
-        display: flex;
-    }
+<style>
+  .options {
+    display: flex;
+  }
 
-    .options button {
-        margin-right: 10px;
-        padding: 5px 10px;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-    }
+  .options button {
+    margin-right: 10px;
+    padding: 5px 10px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
 
 </style>
+
