b4d925da1d16227762aa7bb75bca196170b1d78e
Gazze, Maxime <gazze.maxime@courrier.uqam.ca>
2024-03-25 09:46:02 -0400

    DEBUG profile

--- frontend/src/routes/profile/+page.ts
+++ frontend/src/routes/profile/+page.ts
@@ -2,12 +2,18 @@ import type { PageLoad } from './$types';
 import { API_URL } from '../../constants';
 
 export const load: PageLoad = async ({ fetch }) => {
-  const res = await fetch(`${API_URL}/user`, {
-    credentials: 'include',
-  });
+  try {
+    const res = await fetch(`${API_URL}/user`, {
+      credentials: 'include',
+    });
 
-  return {
-    res,
-    // user: await res.json(),
-  };
+    return {
+      res,
+      // user: await res.json(),
+    };
+  } catch (error: unknown) {
+    return {
+      error,
+    };
+  }
 };
