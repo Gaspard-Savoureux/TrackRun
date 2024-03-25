1240ba375830893bd5a0ceb4b3d0bedc63d0dbfb
J-C <cricriclouatre@gmail.com>
2024-03-25 16:48:36 -0400

    Add new error codes to ErrorCode enum
    Several new error codes have been added to the ErrorCode enumeration in errorCode.ts file. This includes codes for invalid name, city, activity type, date, duration, distance, comment, GPX file, and ID.

--- frontend/src/lib/errorCode.ts
+++ frontend/src/lib/errorCode.ts
@@ -2,14 +2,24 @@
 
 // Enumération des codes d'erreur
 export enum ErrorCode {
+  // eslint-disable-next-line no-unused-vars
   Missing,
+  // eslint-disable-next-line no-unused-vars
   InvalidNom,
+  // eslint-disable-next-line no-unused-vars
   InvalidVille,
+  // eslint-disable-next-line no-unused-vars
   InvalidTypeActivite,
+  // eslint-disable-next-line no-unused-vars
   InvalidDate,
+  // eslint-disable-next-line no-unused-vars
   InvalidDuree,
+  // eslint-disable-next-line no-unused-vars
   InvalidDistance,
+  // eslint-disable-next-line no-unused-vars
   InvalidComment,
+  // eslint-disable-next-line no-unused-vars
   InvalidGPX,
+  // eslint-disable-next-line no-unused-vars
   InvalidId
 }
