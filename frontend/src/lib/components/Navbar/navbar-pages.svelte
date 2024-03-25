396d3e53807ca54e6ffd83255343c14011fe78d1
Dominique Elias <elias.dominique@courrier.uqam.ca>
2024-03-25 19:18:25 -0400

    Add trainer activity route in navbar and todo trainer activites

--- frontend/src/lib/components/Navbar/navbar-pages.svelte
+++ frontend/src/lib/components/Navbar/navbar-pages.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
   import Modal from '$lib/components/informative-modal.svelte';
   import { ActivityIcon } from 'svelte-feather-icons';
+  import { isTrainer } from '$lib/stores/trainer';
 </script>
 
-<Modal href="/activity">
-  <div class="nav-item">
-    <ActivityIcon />
-    <p>Activities</p>
-  </div>
-</Modal>
+{#if $isTrainer}
+  <Modal href="trainer/activity">
+    <div class="nav-item">
+      <ActivityIcon />
+      <p>Activities</p>
+    </div>
+  </Modal>
+{:else}
+  <Modal href="/activity">
+    <div class="nav-item">
+      <ActivityIcon />
+      <p>Activities</p>
+    </div>
+  </Modal>
+{/if}
+
 
 <style>
   .nav-item {
