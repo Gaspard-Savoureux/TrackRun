06fe5413f69bc9976702355decef18638402748e
J-C <cricriclouatre@gmail.com>
2024-03-25 16:41:22 -0400

    Add ErrorCode enum in frontend lib
    A new ErrorCode enumeration has been added to the frontend library to standardize error handling. It includes codes for various types of invalid or missing data, such as name, city, activity type, date, duration, distance, comment, GPX, and ID.

---
+++ frontend/src/lib/errorCode.ts
@@ -0,0 +1,15 @@
+//TODO À changer de repertoire. Ca va etre fait avec le merge de geolocation
+
+// Enumération des codes d'erreur
+export enum ErrorCode {
+  Missing,
+  InvalidNom,
+  InvalidVille,
+  InvalidTypeActivite,
+  InvalidDate,
+  InvalidDuree,
+  InvalidDistance,
+  InvalidComment,
+  InvalidGPX,
+  InvalidId
+}
