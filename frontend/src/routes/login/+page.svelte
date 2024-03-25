374e34a5e3330d897a5f8b9e5a73590b28787510
Lajoie, Samuel <lajoie.samuel@courrier.uqam.ca>
2024-03-25 14:07:15 -0400

    FIX alignment for the login as trainer button

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -3,11 +3,10 @@
   import { page } from '$app/stores';
   import FormNotification from '$lib/components/form-notification.svelte';
   import { isTrainer } from '$lib/stores/trainer';
-  
+
   $: message = $page.url.searchParams.get('message') ?? '';
 
   export let form;
-
 </script>
 
 <svelte:head>
@@ -32,7 +31,7 @@
       />
       <input type="password" placeholder="Enter your password" name="password" />
       <label>
-        <input type="checkbox" name="isTrainer" bind:checked={$isTrainer}/>
+        <input type="checkbox" name="isTrainer" bind:checked={$isTrainer} />
         Log in as a trainer
       </label>
       <button type="submit">Log in</button>
@@ -46,6 +45,11 @@
 </section>
 
 <style>
+  label {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
   section {
     padding: 3rem 1.5rem;
   }
