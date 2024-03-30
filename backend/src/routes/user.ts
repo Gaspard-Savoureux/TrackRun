cc80b788c413db0257b202b312272ce486694c63
Lajoie, Samuel <lajoie.samuel@courrier.uqam.ca>
2024-03-29 22:15:26 -0400

    ADD validation password backend + FIX no error if sex is null

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -53,11 +53,9 @@ const router = express.Router();
 router.post('/',
   [
     body('username').isString().isLength({min: 1, max: 256}).withMessage('username must be a not empty string'),
-    // Password pas trop long pour l'instant pour simplifier démo
-    body('password').isString().isLength({min: 1, max: 72}).withMessage('password must be a min of 8 chars and max 72 chars'),
-    // body('password').isString().isLength({min: 8, max: 72}).withMessage('password must be a min of 8 chars and max 72 chars'),
+    body('password').isString().isLength({min: 8, max: 72}).withMessage('password must be a min of 8 chars and max 72 chars'),
     body('email').isString().isEmail(),
-    body('name').isLength({min: 1}).isString()
+    body('name').isLength({min: 1}).isString().withMessage('Name must be an non empty string')
   ],
   expressValidator,
   createUser
@@ -217,7 +215,7 @@ router.put('/',
     body('age').custom(isGivenTypeOrNull(evTypes.INT)).withMessage('Required, must be numerical value'),
     body('height').custom(isGivenTypeOrNull(evTypes.FLOAT)).withMessage('Required, must be numerical. The value is in cm'),
     body('weight').custom(isGivenTypeOrNull(evTypes.FLOAT)).withMessage('Required. Must be numerical. The value is in kg'),
-    body('sex').isString().matches(/\b(?:Homme|Femme|Autre)\b/).withMessage('Required, must be either Homme, Femme or Autre'),
+    body('sex').custom(isGivenTypeOrNull(evTypes.SEX)).withMessage('Required, must be either Homme, Femme or Autre'),
     body('description').custom(isGivenTypeOrNull(evTypes.STRING)).isLength({ min: 0, max: 1024 }).withMessage('Required, string with max length of 1024')
   ],
   expressValidator,
